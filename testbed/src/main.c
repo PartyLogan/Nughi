3278b87c46b0da8944ac5c1ddf41653e3c33c681
PartyLogan <l.nicol.dickson@gmail.com>
2022-06-29 21:23:28 +1000

    did some minor changes to get the events to work

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -14,8 +14,7 @@ int main(void) {
 
     platform_state state;
     if (platform_startup(&state, "Nughi Engine Testbed", 100, 100, 1280, 720)) {
-        while (TRUE) {
-            platform_pump_messages(&state);
+        while (platform_pump_messages(&state)) {
         }
     }
     platform_shutdown(&state);
